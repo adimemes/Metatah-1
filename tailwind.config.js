2b59c94fcad3b12651530c945bef10178f0916a5
adimemes <adimemes06@gmail.com>
2025-01-16 23:15:20 +0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "node_modules/preline/dist/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bonheur: ['"Bonheur Royale"', "cursive"], // Font Bonheur Royale
+        lexend: ['"Lexend"', "sans-serif"], // Font Lexend
+      },
+    },
+  },
+  plugins: [require("preline/plugin"),require("daisyui")],
+};
